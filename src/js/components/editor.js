dfb2e540c4c3f119fa4579c7941d09496d4b75d6
Greg Poole <gregory.poole@sydney.edu.au>
2015-10-16 00:45:19 +1100

    Finished off setting up the input form

--- src/js/components/editor.js
+++ src/js/components/editor.js
@@ -8,19 +8,23 @@ export class Editor extends React.Component {
 				<h2>hCard Builder</h2>
 				<fieldset>
 					<legend>Personal details</legend>
-					<TextField name="givenName" placeholder="Sam" label="Given name" />
-					<TextField name="surname" placeholder="Fairfax" label="Surname" />
-					<TextField name="email" placeholder="sam.fairfax@fairfaxmedia.com.au" label="Email" pattern={/^[^@]+@[^\.]+\./} />
-					<TextField  name="phone" placeholder="00 0000 0000" label="Phone" />
+					<div className="input-set">
+						<TextField name="givenName" placeholder="First" label="Given name" className="short-input" />
+						<TextField name="surname" placeholder="Last" label="Surname" className="short-input" />
+						<TextField name="email" placeholder="email@domain.com" label="Email" pattern={/^[^@]+@[^\.]+\./} className="short-input" />
+						<TextField  name="phone" placeholder="00 0000 0000" label="Phone" className="short-input" />
+					</div>
 				</fieldset>
 				<fieldset>
 					<legend>Address</legend>
-					<TextField name="house" placeholder="Sam" label="House name or #" />
-					<TextField name="street" placeholder="Sam" label="Street" />
-					<TextField name="suburb" placeholder="Sam" label="Suburb" />
-					<TextField name="state" placeholder="Sam" label="State" />
-					<TextField name="postcode" placeholder="Sam" label="Postcode" />
-					<TextField name="country" placeholder="Sam" label="Country" />
+					<div className="input-set">
+						<TextField name="house" placeholder="#" label="House name or #" className="short-input" />
+						<TextField name="street" placeholder="Street name" label="Street" className="short-input" />
+						<TextField name="suburb" placeholder="Suburb" label="Suburb" className="short-input" />
+						<TextField name="state" placeholder="State" label="State" className="short-input" />
+						<TextField name="postcode" placeholder="0000" label="Postcode" className="short-input" />
+						<TextField name="country" placeholder="Country" label="Country" className="short-input" />
+					</div>
 				</fieldset>
 				<button className="btn btn-default">Upload avatar</button>
 				<button className="btn btn-primary">Create hCard</button>
