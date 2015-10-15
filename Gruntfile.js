053b285e57321c7372b9da85fe3bea2d5a03c042
Greg Poole <gregory.poole@sydney.edu.au>
2015-10-15 22:04:33 +1100

    Base project

---
+++ Gruntfile.js
@@ -0,0 +1,55 @@
+module.exports = function (grunt) {
+
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		sass: {
+			dist: {
+				files: {
+					'dist/css/app.css': [ 'src/scss/app.scss' ]
+				}
+			}
+		},
+		babel: {
+			dist: {
+				files: [{
+					expand: true,
+					dest: 'dist/',
+					cwd: 'src/',
+					src: 'js/**/*.js'
+				}]
+			}
+		},
+		watch: {
+			files: [ 'src/**/*', 'index.html' ],
+			tasks: [ 'sass', 'babel' ],
+			options: {
+				livereload: true
+			}
+		},
+		connect: {
+			server: {
+				options: {
+					livereload: true
+				}
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-babel');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-connect');
+
+	// Default task(s).
+	grunt.registerTask('default', [
+		'sass',
+		'babel'
+	]);
+
+	grunt.registerTask('serve', [
+		'connect',
+		'watch'
+	]);
+
+};
